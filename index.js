27457ef1df4a48296d62e1aa1788c7291d32bdf2
rehanulhb <rehanulh@gmail.com>
2025-01-19 03:14:59 +0100

    Delete API Done for Backend

--- index.js
+++ index.js
@@ -67,6 +67,15 @@ async function run() {
 
     });
 
+    
+
+    app.delete('/bookings/:id', async(req, res)=>{
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)}
+      const result = await bookingCollection.deleteOne(query);
+      res.send(result);
+    })
+
 
 
     // Send a ping to confirm a successful connection
