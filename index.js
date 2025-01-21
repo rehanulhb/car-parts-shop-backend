930636d29cbf0708946d6602dc46ddd4594ad456
rehanulhb <rehanulh@gmail.com>
2025-01-21 22:04:43 +0100

    Installed JSON web Token

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 require('dotenv').config()
 const app = express();
 const port = process.env.PORT || 5000;
