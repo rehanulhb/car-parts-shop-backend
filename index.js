c2bc37349f1429b41a8f63b95d4f5437e5053a08
rehanulhb <rehanulh@gmail.com>
2025-01-22 00:50:54 +0100

    Installed Cookie Parser

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ async function run() {
     app.post('/jwt', async(req, res)=>{
       const user = req.body;
       console.log(user);
-      const token =jwt.sign(user, 'secret', {expiresIn: '1h'})
+      const token =jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {expiresIn: '1h'})
       res.send(token)
     })
 
