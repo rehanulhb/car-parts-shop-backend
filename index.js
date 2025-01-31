54c106e350f2ecdb21ed608af2d815448629f7ef
rehanulhb <rehanulh@gmail.com>
2025-01-31 02:06:12 +0100

    Solving problem regarding noe showing the token on Application

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
 const express = require('express');
 const cors = require('cors');
 const jwt = require('jsonwebtoken');
-const cookieParser = require('cookie-parser');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const app = express();
 const port = process.env.PORT || 5000;
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-
 
 
 //middleware
 app.use(cors({
-  origin: ['http://localhost:5173'],
+  origin:[
+    'http://localhost:5173/'
+  ],
   credentials: true
 }));
 app.use(express.json());
-app.use(cookieParser());
+
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.lsofw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
@@ -29,35 +29,6 @@ const client = new MongoClient(uri, {
   }
 });
 
-//middleware
-const logger = async(req, res, next) =>{
-  console.log('Called', req.host, req.originalUrl);
-  next();
-} 
-
-
-const verifyToken = async(req, res, next)=>{
-  const token = req.cookies?.token;
-  console.log('Value of Token in Middleware', token)
-  if(!token){
-    return res.status(401).send({message: 'Not Authorized'})
-  }
-  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded)=>{
-    //error
-    if(err){
-      console.log(err)
-      return res.status(401).send({message: 'Unauhorized'})
-    }
-
-    //if token is valid then it would be decoded
-    console.log('Value in the Token', decoded)
-    req.user = decoded;
-    next();
-  })
-  
-}
-
-
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
@@ -67,23 +38,16 @@ async function run() {
     const bookingCollection = client.db('carShop').collection('bookings')
 
     //Auth API
-    app.post('/jwt', logger,  async(req, res)=>{
+    app.post('/jwt', async(req, res)=>{
       const user = req.body;
-      console.log('User for token',user);
-      const token =jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {expiresIn: '1h'})
+      console.log('User for token', user);
+      const token =jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {expiresIn: '1h'});
 
-      res
-      .cookie('token', token, {
-        httpOnly: true,
-        secure: false,
-        
-        
-      })
-      .send({success: true})
+      res.send({token})
     })
 
     //Service API
-    app.get('/services', logger,  async(req, res) => {
+    app.get('/services',  async(req, res) => {
         const cursor = serviceCollection.find();
         const result = await cursor.toArray();
         res.send(result)
@@ -101,22 +65,12 @@ async function run() {
 
     //Bookings
 
-    app.get('/bookings',logger,verifyToken, async(req, res)=>{
-      console.log(req.query.email);
-      //console.log('Tok tok token', req.cookies.token)
-      console.log('User in the valid token', req.user)
-
-        if(req.query.email !== req.user.email){
-          return res.status(403).send({message: 'forbidden Access'})
-        }
-
-        let query = {}
-        if(req.query?.email){
-          query = {email: req.query.email}
-        }
-        const result = await bookingCollection.find(query).toArray();
-        res.send(result);
-    })
+    app.post('/bookings', async (req, res) => {
+      const booking = req.body;
+      console.log(booking);
+      const result = await bookingCollection.insertOne(booking);
+      res.send(result);
+  });
 
     app.post('/bookings', async(req, res)=>{
         const booking = req.body;
